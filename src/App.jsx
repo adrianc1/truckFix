891a8d9e4684cee8a91be40887a388fec1102c20
adriansito <adriancrosby02@gmail.com>
2025-07-09 22:13:22 -0700

    restarted project to have clean slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,8 @@
-import { useState } from 'react';
-import { useNavigate, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
-import Hero from './components/Hero/Hero';
-import NearbyShopsMap from './components/MapPage';
-import Cards from './Cards';
+import { useState, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
-import './App.css';
-
-function App() {
-	const navigate = useNavigate();
-
-	function useCurrentLocation() {
-		console.log('ive been clicked!');
-		// Get current location
-		navigator.geolocation.getCurrentPosition((position) => {
-			setCurrentLocation({
-				lat: position.coords.latitude,
-				lng: position.coords.longitude,
-			});
-		});
-	}
-
-	return (
-		<div className="flex flex-col h-full">
-			<Nav />
-			<Routes>
-				{/* Home page route shows Hero + Cards */}
-				<Route
-					path="/"
-					element={
-						<>
-							<Hero navigate={navigate} />
-							<Cards />
-						</>
-					}
-				/>
-
-				{/* Map route shows only map */}
-				<Route path="/map" element={<NearbyShopsMap />} />
-			</Routes>
-		</div>
-	);
-}
+const App = () => {
+	return <></>;
+};
 
 export default App;
