f5076f7e3d516be84813c70e7ee518a42b8a288f
adriansito <adriancrosby02@gmail.com>
2025-07-12 13:35:46 -0700

    added icons to the search bars and current location text

--- src/RepairSearchForm.jsx
+++ src/RepairSearchForm.jsx
@@ -1,3 +1,5 @@
+import { Wrench, Navigation, MapPin } from 'lucide-react';
+
 export default function RepairSearchForm() {
 	const shopCategories = [
 		'All Services',
@@ -9,20 +11,33 @@ export default function RepairSearchForm() {
 	return (
 		<form className="w-full flex flex-col justify-center px-2 gap-4">
 			<div className="flex flex-col">
-				<input
-					type="text"
-					className="border w-full rounded-xl py-2 px-4 mt-4"
-					placeholder="Enter City / Town"
-				/>
-				<span className="self-end">
+				{/* Location Search Bar */}
+				<div className="relative mt-4">
+					<input
+						type="text"
+						className="border w-full rounded-xl py-2 pl-10 pr-4"
+						placeholder="Enter City / Town"
+					/>
+					<MapPin className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400 pointer-events-none" />
+				</div>
+				{/* Get current location  */}
+				<span className="self-end flex justify-center items-center">
+					<Navigation className="w-4" />
 					<a href="">Use Current Location</a>
 				</span>
 			</div>
-			<input
-				type="text"
-				className="border w-full rounded-xl py-2 px-4 mb-2"
-				placeholder="What Service are you looking for?"
-			/>
+
+			{/* Type of repair shop search bar */}
+			<div className="relative">
+				<input
+					type="text"
+					className="border w-full rounded-xl py-2 pl-10 pr-4"
+					placeholder="Engine, Brakes, Repair..."
+				/>
+				<Wrench className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400 pointer-events-none" />
+			</div>
+
+			{/* Repair shop categories  */}
 			<div className="flex overflow-x-scroll w-full gap-2 px-2 pb-4">
 				{shopCategories.map((cat) => (
 					<button className="border border-gray-300 rounded-4xl px-4 py-2 min-w-max">
--- src/ShopList.jsx
+++ src/ShopList.jsx
@@ -1,3 +1,5 @@
+import star from './assets/images/star.svg';
+
 export default function ShopList() {
 	const shops = [
 		{
@@ -5,31 +7,39 @@ export default function ShopList() {
 			shopAddress: '1234 Truck Hwy, Dallas, TX',
 			shopDistance: '0.8 miles',
 			shopTags: ['Engine', 'Brakes', '24/7'],
+			shopRating: 4.1,
+			isOpen: 'Open Now',
 		},
 		{
 			shopName: 'Interstate Truck Service',
 			shopAddress: '5678 Freeway Rd, Dallas, TX',
 			shopDistance: '1.2 miles',
 			shopTags: ['Tires', 'Electrical'],
+			shopRating: 4.7,
+			isOpen: 'Open Now',
 		},
 		{
 			shopName: 'Diesel Doctors',
 			shopAddress: '910 Industrial Blvd, Dallas, TX',
 			shopDistance: '2.5 miles',
 			shopTags: ['Engine', 'Diagnostics', 'Parts'],
+			shopRating: 3.8,
+			isOpen: 'Closed',
 		},
 	];
 
 	return (
-		<section className="w-full h-auto px-2">
-			<h2 className="nearby-shops font-bold text-2xl">Nearby Repair Shops</h2>
+		<section className="w-full h-auto px-2 mt-8">
+			<h2 className="nearby-shops mb-4 font-bold text-2xl">
+				Nearby Repair Shops
+			</h2>
 			<ul className="">
 				{shops.map((shop) => (
 					<li
 						key={shop.shopName}
-						className="px-3 py-4  border-2 border-gray-200 rounded h-auto mb-0.5"
+						className="flex px-3 py-4 border-2 border-gray-200 rounded-xl h-auto mb-0.5"
 					>
-						<div className="shop-info flex flex-col">
+						<div className="shop-info w-full flex flex-4 flex-col">
 							<h5 className="repair-shop-name font-bold text-lg mb-1">
 								{shop.shopName}
 							</h5>
@@ -45,6 +55,15 @@ export default function ShopList() {
 									</span>
 								))}
 							</span>
+						</div>
+						<div className="rating-status-container flex-1 w-full flex flex-col justify-center items-center">
+							<span className="rating flex justify-center items-center gap-1">
+								<div className="star w-1/5">
+									<img src={star} alt="" />
+								</div>
+								{shop.shopRating}
+							</span>
+							<span className="rating">{shop.isOpen}</span>
 						</div>
 					</li>
 				))}
