c8edc629542afdcee7855de7fae2a33138e7b957
adriansito <adriancrosby02@gmail.com>
2025-07-12 00:30:57 -0700

    created the map widget container with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './Header';
 import RepairSearchForm from './RepairSearchForm';
+import MapWidget from './MapWidget';
 const App = () => {
 	return (
 		<>
 			<Header />
 			<RepairSearchForm />
+			<MapWidget />
 		</>
 	);
 };
--- src/MapWidget.jsx
+++ src/MapWidget.jsx
@@ -1,8 +1,14 @@
 export default function MapWidget() {
 	return (
-		<div>
-			<div className="map-container"></div>
-			<div className="location-container"></div>
+		<div className="flex flex-col mx-auto items-center mt-4 w-full px-2 ">
+			<div className="map-container border rounded-t-xl w-full h-48 bg-gray-300">
+				Loading Map...
+			</div>
+			<div className="location-container border rounded-b-xl w-full h-24 ">
+				<h5 className="font-bold">Location</h5>
+				<span className="text-gray-400">Dallas, TX (25 mile radius)</span>
+				<button className="border ml-24 rounded-xl">Change Radius</button>
+			</div>
 		</div>
 	);
 }
