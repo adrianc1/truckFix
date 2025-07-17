87201137d501cd99b417191549edafe1b913ba12
adriansito <adriancrosby02@gmail.com>
2025-07-16 23:52:24 -0700

    added default zoom and default center to the map widget

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -52,8 +52,8 @@ export default function Results({ currentLocation }) {
 				<Map
 					style={{ width: '100vw', height: '100vh' }}
 					mapId={import.meta.env.VITE_MAP_ID}
-					center={{ lat: 36.1718, lng: -115.1458 }}
-					zoom={9}
+					defaultCenter={{ lat: 36.1718, lng: -115.1458 }}
+					defaultZoom={9}
 					gestureHandling={'greedy'}
 					disableDefaultUI={true}
 				/>
