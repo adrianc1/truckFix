de569ce2aa29ff3a5288704a040455b85e9226da
adriansito <adriancrosby02@gmail.com>
2025-10-01 14:51:30 -0700

    updated the footer and header logo and text

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,10 @@
-import { Truck } from 'lucide-react';
+import Truck from '../../assets/images/TruckFix.png';
 const Footer = () => {
 	return (
 		<footer className="w-full flex flex-col justify-center gap-8 items-center border-t border-gray-300 mt-4">
 			<div className="flex gap-2 mt-4">
-				<Truck color="#ff6900" size={32} />
-				<h6 className="text-2xl font-bold">RepairFind</h6>
+				<img src={Truck} alt="" className="h-32 w-32" />
+				{/* <h6 className="text-2xl font-bold">TruckFix</h6> */}
 			</div>
 			<ul className="w-2/3 flex justify-around">
 				<li>Terms</li>
@@ -12,7 +12,7 @@ const Footer = () => {
 				<li>Cookies</li>
 			</ul>
 			<span className="text-gray-400 mb-2">
-				&copy; 2025 RepairFind. All rights reserved.
+				&copy; 2025 TruckFix. All rights reserved.
 			</span>
 		</footer>
 	);
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,9 +7,8 @@ const Header = () => {
 	return (
 		<header className="fixed mb-24 z-200 top-0 left-0 right-0  flex justify-between items-center py-1 px-4 bg-white/80 backdrop-blur-sm border-b border-gray-300/60">
 			<Link to="/" className="flex items-center gap-1 lg:pl-24">
-				<img src={Truck} alt="TruckFix Logo" className="w-20 h-20 m-0 p-0" />
-				{/* <Truck color="#ff6900" size={32} /> */}
-				{/* <h1 className="text-2xl text-center font-bold">TruckFix</h1> */}
+				<img src={Truck} alt="TruckFix Logo" className="w-16 h-16 m-0 p-0" />
+				<h1 className="text-2xl text-center font-bold">TruckFix</h1>
 			</Link>
 
 			<div
